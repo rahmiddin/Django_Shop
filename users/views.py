cc0a569b1d7ab0e3c82c8f5901d5084aea3815a0
Rahmidin <rahmiddin.hasanov@bk.ru>
2023-04-13 13:52:50 +0500

    ADD_registration/login/logout_ADD_profile-page/admin-panel

---
+++ users/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.contrib import auth, messages
+# Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def registration(requset):
+    if requset.method == 'POST':
+        form = UserRegistrationForm(data=requset.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(requset, 'Вы зарегестрировались')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(requset, 'users/register.html', context=context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'Store - Profile',
+        'form': form
+    }
+    return render(request, 'users/profile.html', context=context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
